56904057f23d05df960f65dffd84a06b4e891034
Rexbyy <glpro88@gmail.com>
2025-02-22 04:01:52 +0700

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -1,7 +1,20 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+body{
+  padding: 25px;
 }
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+.title {
+	color: #5C6AC4;
+}
+.volume-container {
+    position: absolute;
+    top: 20px; /* Adjust this value for vertical positioning */
+    left: 20px; /* Adjust this value for horizontal positioning */
+    background: rgba(15, 15, 80, 0.6);
+    padding: 10px;
+    border-radius: 10px;
+    color: white;
+    z-index: 1000; /* Ensure it appears above other elements */
+}
+html, body {
+    height: 100%;
+    margin: 0;
+}
