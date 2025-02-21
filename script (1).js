cd6588a3f67958bfa9b5c3c09e3114e73a3c41fa
Rexbyy <glpro88@gmail.com>
2025-02-22 04:19:19 +0700

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -5,17 +5,7 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
-document.getElementById("fullscreenButton").onclick = function() {
-    if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-        document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-        document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-        document.documentElement.msRequestFullscreen();
-    }
-};
+
 window.onload = function() {
     createRain();
 };
